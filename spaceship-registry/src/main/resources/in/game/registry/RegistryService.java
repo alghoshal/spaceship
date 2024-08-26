fd10498a960263856594f15df35548d7258575f6
algo <alghoshal@gmail.com>
2024-08-27 00:08:30 +0530

    Registry service

--- spaceship-registry/src/main/resources/in/game/registry/RegistryService.java
+++ spaceship-registry/src/main/resources/in/game/registry/RegistryService.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class XLRegistryService {
+public class RegistryService {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(XLRegistryService.class, args);
+		SpringApplication.run(RegistryService.class, args);
 	}
 
 }
